eeb1cc21cb614db37af56bb702f9bb92548c2123
MD ALIF BABU <alif@k53tech.com>
2024-11-19 15:25:02 +0600

    open ai tested

--- src/main/java/com/example/openai/ChatController.java
+++ src/main/java/com/example/openai/ChatController.java
@@ -1,23 +1,30 @@
 package com.example.openai;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping
 public class ChatController {
+    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
     private final ChatClient chatClient;
 
     public ChatController(ChatClient.Builder chatClient) {
         this.chatClient = chatClient.build();
     }
 
-    @GetMapping("story/{ask}")
-    Map<String, String> story(@PathVariable String ask){
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello there!";
+    }
+
+    @GetMapping("/story/{ask}")
+    Map<String, String> story(@PathVariable(value = "ask") String ask){
+        log.info("What is the problem?");
         var response = this.chatClient
                 .prompt()
                 .user(ask)
